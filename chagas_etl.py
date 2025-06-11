47878b382328cc53cf48c5f69e2d94c747d74b60
Marcos Fellipe Silva de Oliveira <marcosf1625@gmail.com>
2025-06-11 08:29:12 -0300

    Update chagas_etl.py

--- chagas_etl.py
+++ chagas_etl.py
@@ -2,6 +2,7 @@ from numpy import nan
 import pandas as pd
 from functools import reduce
 from datetime import datetime
+from rapidfuzz import process, fuzz
 
 from utilities import Utilities
 
@@ -19,7 +20,27 @@ class Chagas:
                       )
 
     def transform(self,df:pd.DataFrame):
-        # tratando valores que estão vazios
+        # Ajeita as Siglas #NUMERO ESTADO DO PARÁ = 15
+        df_aux_mun = self.util.extract_dim_municipios()
+
+        df['ID_MUNICIP'] = df['ID_MUNICIP'].astype(str)
+
+        df['ID_MUNICIP'] = df["ID_MUNICIP"].apply(
+            lambda x: self._fuzzy_match(x, df_aux_mun['CD_MUN'].tolist()))
+
+        #Caso queira pelo numero do estado enves da sigla - descomentar esta linha e comentar a outra
+        #uf_dict = df_aux_mun[['CD_UF', 'SIGLA_UF']].drop_duplicates().set_index('SIGLA_UF')['CD_UF'].to_dict()
+        uf_dict = df_aux_mun[['CD_UF', 'SIGLA_UF']].drop_duplicates().set_index('CD_UF')['SIGLA_UF'].to_dict()
+
+        df['SG_UF_NOT'] = df['SG_UF_NOT'].map(uf_dict).fillna(df['SG_UF_NOT'])
+        df['SG_UF'] = df['SG_UF'].map(uf_dict).fillna(df['SG_UF'])
+
+        #Filtra Apenas o PA
+        df = df[df['SG_UF_NOT'] == 'PA']
+
+        df = pd.merge(df,df_aux_mun,left_on='ID_MUNICIP',right_on='CD_MUN',how='left')
+
+        #tratando valores que estão vazios
         df['CS_RACA'] = self.util.remove_nan_null(df, col_name='CS_RACA')
 
         df['CRITERIO'] = self.util.remove_nan_null(df, col_name='CRITERIO')
@@ -43,36 +64,40 @@ class Chagas:
         df['CLASSI_FIN'] = df['CLASSI_FIN'].astype(str).map(self.util.category_classi_final).fillna(df['CLASSI_FIN'])
 
         # Calcula a idade
-        current_year = datetime.now().year
-        df['idade'] = df['ANO_NASC'].replace(" ", nan)
+        ano_atual = datetime.now().year
+        df['ANO_NASC'] = (
+            df['ANO_NASC']
+            .astype(str)
+            .str.strip()
+            .replace('', pd.NA)
+        )
         df['ANO_NASC'] = pd.to_numeric(df['ANO_NASC'], errors='coerce')
-        df['idade'] = current_year - df['ANO_NASC']
-
-        # Ajeita as Siglas #NUMERO ESTADO DO PARÁ = 15
-        df_aux_mun = self.util.extract_dim_municipios()[['uf', 'sigla']].drop_duplicates()
-
-        #Caso queira pelo numero do estado enves da sigla - descomentar esta linha e comentar a outra
-        # uf_dict = df_aux_mun.set_index('sigla')['uf'].to_dict()
-        uf_dict = df_aux_mun.set_index('uf')['sigla'].to_dict()
-
-        df['SG_UF_NOT'] = df['SG_UF_NOT'].map(uf_dict).fillna(df['SG_UF_NOT'])
-        df['SG_UF'] = df['SG_UF'].map(uf_dict).fillna(df['SG_UF'])
+        df['idade'] = ano_atual - df['ANO_NASC']
 
         # Corrige tipos de coluna problemáticas antes de salvar com pyarrow
-        for col in df.select_dtypes(include='object').columns:
-            df[col] = df[col].where(df[col].notna(), None).astype("string")
+        df = df.convert_dtypes()
+
+        df.drop(columns=['CD_MUN','NM_RGINT','CD_UF','SIGLA_UF'],inplace=True)
+
+        for col in ['CLASSI_FIN', 'CRITERIO', 'EVOLUCAO', 'CS_RACA']:
+            df[col] = df[col].astype("string")
 
         return df
 
     def load(self,df:pd.DataFrame) ->None:
-        df.to_parquet("chagas_consolidado.parquet", index=False, engine='pyarrow')
-
+        df.to_csv("chagas_consolidado_PA.csv", index=False, sep=';')
+        print("deu bom")
     def execute(self):
         df = self.extract()
 
         df = self.transform(df)
 
         self.load(df)
+
+    def _fuzzy_match(self, cod_mun, lista_municipios):
+        match, score, idx = process.extractOne(cod_mun, lista_municipios, scorer=fuzz.token_sort_ratio)
+        return match if score >= 75 else None
+
 
 if __name__ == '__main__':
     Chagas().execute()
